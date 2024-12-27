fddf74c265a11ee5e9df99ad71fc814c1144fafc
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2024-12-27 15:13:50 +0500

    practice 3

---
+++ Practice 3/app.js
@@ -0,0 +1,19 @@
+// Question 1
+let studentNames = [];
+
+// Question 2
+studentNames["student1"] = "Ahmed";
+studentNames["student2"] = "Rehman";
+
+// Question 3
+let studentNames = ["Ahmed", "Rehman", "Anas", "Waqar"];
+
+// Question 4
+let numbers = [10, 20, 30, 40, 50];
+
+// Question 5
+let booleanArray = [true, false, true, false, true];
+
+// Question 6
+let mixedArray = [42, "Hello", true, 3.14, "World"];
+
