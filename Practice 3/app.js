bd438a5d688e1062cd8fcd82dc2ea66f4473918a
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2025-01-03 01:22:13 +0500

    update

--- Practice 3/app.js
+++ Practice 3/app.js
@@ -7,6 +7,15 @@ studentNames["student2"] = "Rehman";
 
 // Question 3
 let studentNames = ["Ahmed", "Rehman", "Anas", "Waqar"];
+// studentName.push
+studentNames.push("Ali");
+studentNames.shift("Hassan");
+studentNames.unshift("Hassan");
+studentNames.pop("Hassan");
+
+// splice will delete the element at the specified index
+studentNames.splice(2, 1, "Hassan");
+
 
 // Question 4
 let numbers = [10, 20, 30, 40, 50];
@@ -17,3 +26,14 @@ let booleanArray = [true, false, true, false, true];
 // Question 6
 let mixedArray = [42, "Hello", true, 3.14, "World"];
 
+// Question 7
+let qualification = ["SSC", "HSC", "BSC", "BS", "PhD"];
+document.write(`<h1>Qualification</h1></br>
+    ${qualification[0]}
+    `);
+
+// Question 8
+let studentNames = ["Ahmed", "Rehman", "Anas", "Waqar"];
+let score = [85, 90, 78, 92];
+let totalMarks = 100;
+document.write(`Score of ${studentNames[0]} is ${score[0]} out of ${totalMarks}.`);
---
+++ Practice 6/app.js
@@ -0,0 +1,13 @@
+// //Template String
+// let str = 'ahmed';
+// console.log(`${str[0].toUpperCase()}${str.slice(1)}`) // Output: "Ahmed"
+
+// let arr = ["Karachi", "Lahore", "Islamabad"];
+// console.log(`${arr.reverse()}`);
+// let arr1 = arr.slice(0, 2).reverse();
+// console.log(`${arr1} ${arr[2]}`); // Fixed the bug by changing arr(2) to arr[2]
+
+
+// let arc = ["Karachi", "Lahore", "Islamabad"];
+// arc = arc.reverse();
+// console.log(`${arc}`);
