6b7f3ae944a8286bc06cb18b958e2b9b9d965145
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2025-01-03 10:43:59 +0500

    Update app.js

--- Practice 8/app.js
+++ Practice 8/app.js
@@ -1,48 +1,48 @@
+// // // function xyz(b, c) { // parameters
+// // //     let a = 10;
+// // //     console.log(a+b+c); // 30
+// // // }
+
+// // // xyz(10,20); // arguments  
+
+
+// // // _____________________________________________________________________________________
+
+
 // // function xyz(b, c) { // parameters
 // //     let a = 10;
-// //     console.log(a+b+c); // 30
+// //    return a+b+c; // 30
+// //    alert('hello'); // wont be executed because of return statement
 // // }
 
-// // xyz(10,20); // arguments  
-
+// // let result = xyz(10,20); // arguments  
+// // console.log('result '+result); // 30
 
 // // _____________________________________________________________________________________
 
 
-// function xyz(b, c) { // parameters
-//     let a = 10;
-//    return a+b+c; // 30
-//    alert('hello'); // wont be executed because of return statement
+// function xyz(b, c, o) { // parameters
+//     b = 20; // 20 will be used instead of 10 
+//    if (o === '+') {
+//        return b+c;
+//    } else if (o === '-') {
+//        return b-c;
+//    } else if (o === '*') {
+//        return b*c;
+//    } else if (o === '/') {
+//        return b/c;
+//    } else {
+//        return 'Invalid Operator';
+//    }
 // }
-
-// let result = xyz(10,20); // arguments  
-// console.log('result '+result); // 30
-
-// _____________________________________________________________________________________
-
-
-function xyz(b, c, o) { // parameters
-    b = 20; // 20 will be used instead of 10 
-   if (o === '+') {
-       return b+c;
-   } else if (o === '-') {
-       return b-c;
-   } else if (o === '*') {
-       return b*c;
-   } else if (o === '/') {
-       return b/c;
-   } else {
-       return 'Invalid Operator';
-   }
-}
-let plus = xyz(10,20,'+');
-let minus = xyz(10,20,'-');
-let multiply = xyz(10,20,'*');
-let divide = xyz(10,20,'/');
-let invalid = xyz(10,20,'$');
-console.log('plus '+plus);
-console.log('minus '+minus);
-console.log('multiply '+multiply);
-console.log('divide '+divide);
-console.log('invalid '+invalid);
+// let plus = xyz(10,20,'+');
+// let minus = xyz(10,20,'-');
+// let multiply = xyz(10,20,'*');
+// let divide = xyz(10,20,'/');
+// let invalid = xyz(10,20,'$');
+// console.log('plus '+plus);
+// console.log('minus '+minus);
+// console.log('multiply '+multiply);
+// console.log('divide '+divide);
+// console.log('invalid '+invalid);
 
