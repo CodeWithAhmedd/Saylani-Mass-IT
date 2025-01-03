4911c75ccb68758af0372b73f48b53af10766c1c
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2025-01-03 10:55:25 +0500

    Update app.js

--- Practice 7/app.js
+++ Practice 7/app.js
@@ -1,1 +1,12 @@
 // Question 1
+let orders = [1, 2, 3, 5];
+
+function findMissingNumber(arr) {
+    let n = arr.length + 1;
+    let total = (n * (n + 1)) / 2;
+    let sum = arr.reduce((a, b) => a + b, 0);
+    return total - sum;
+}
+
+let missingNumber = findMissingNumber(orders);
+console.log("Missing number is:", missingNumber);
